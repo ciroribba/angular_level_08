67efc58affedf91c4e2481e7473cf25b12c58ab0
ciroribba <ciro.ribba@gmail.com>
2026-09-22 10:45:56 -0300

    add pipe images

--- src/app/products/components/product-card/product-card.ts
+++ src/app/products/components/product-card/product-card.ts
@@ -2,14 +2,13 @@ import { SlicePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '@products/components/interfaces/product.interface';
+import { ProductImagePipe } from '@products/pipes/product-images.pipes';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink, SlicePipe],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.html',
 })
 export class ProductCard {
   product = input.required<Product>();
-
-  imageUrl = computed(() => `http://localhost:3000/api/files/product/${this.product().images[0]}`);
 }
