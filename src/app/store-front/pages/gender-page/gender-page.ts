31a73231e1f555caa6a182344ed2c4537e71eff7
ciroribba <ciro.ribba@gmail.com>
2026-09-22 12:43:39 -0300

    add function gender pages

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductService } from '@products/services/product.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage { 
+  route = inject(ActivatedRoute);
+  productService  = inject(ProductService)
+
+  
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({gender}) => gender))
+  );
+  productsResource = rxResource({
+    params: () => ({
+      gender: this.gender()
+    }),
+  
+    stream: ({ params }) =>
+      this.productService.getProducts({
+        gender: params.gender
+      }),
+  });
+}
