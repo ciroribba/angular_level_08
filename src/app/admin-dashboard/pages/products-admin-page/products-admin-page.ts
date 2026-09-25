d5a1ec7ab5d1890a27fb1fd0326fd0a0909be517
ciroribba <ciro.ribba@gmail.com>
2026-09-25 12:29:49 -0300

    charge products into table admin dashboard

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductTable } from '@products/components/product-table/product-table';
+import { ProductService } from '@products/services/product.service';
+import { Pagination } from '@shared/components/pagination/pagination';
+import { PaginationService} from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
 })
-export class ProductsAdminPage {}
+export class ProductsAdminPage {
+  productService  = inject(ProductService);
+  paginationService = inject(PaginationService);
+
+  productsPerPage = signal<number>(10);
+  
+  productsResource = rxResource({
+    params: () => ({
+      offset: ((this.paginationService.currentPage() - 1) * 9),
+      limit: this.productsPerPage(),
+    }),
+    stream: ({ params }) => this.productService.getProducts({ offset: params.offset, limit: params.limit }),
+  });
+}
